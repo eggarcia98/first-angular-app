c1e3c3fc309561d72eb2915be7fd36ca0def4c03
Erick Garcia <eggarcia9814@gmail.com>
2023-07-07 23:40:11 -0500

    Adding http server

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -108,21 +108,25 @@ export class HousingService {
     },
   ];
 
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
-  }
+  url = 'http://localhost:3000/locations';
 
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(
-      (housingLocation) => housingLocation.id === id
-    );
+  async getHousingLocationById(
+    id: number
+  ): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return (await data.json()) ?? {};
   }
-
+  
   submitApplication(firstName: string, lastName: string, email: string) {
     console.log(
       `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`
     );
   }
-  
+
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return (await data.json()) ?? [];
+  }
+
   constructor() {}
 }
